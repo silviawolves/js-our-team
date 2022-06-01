185e39551aeaf712431c98ab9e6817d14c1b5eb3
silviawolves <silviacanepa.w@gmail.com>
2022-06-01 13:20:02 +0200

    base + consegna

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+
+
+/*
+Per la pagina ‘Chi siamo’ del nostro sito aziendale, dobbiamo sviluppare la sezione “Il nostro team”.
+Definiamo un array di oggetti che rappresentano i membri del team.
+Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+Milestone 1:
+stampare in console l’elenco dei membri del team, scrivendo separatamente i dettagli di ciascun componente.
+Milestone 2:
+stampare i dati all’interno di un contenitore nella pagina html in modo dinamico, creando per ciascun membro del team un elemento html che conterrà i suoi dati.
+BONUS:
+stilizziamo la sezione realizzando le card di ciascun membro del team, come nel mockup allegato.
+*/
