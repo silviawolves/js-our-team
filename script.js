5492d84fe7bb7b493b2909a9ec7df853a6fa7351
silviawolves <silviacanepa.w@gmail.com>
2022-06-01 14:41:20 +0200

    milestone 1

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 'use strict'
 
+const membriTeam = [
+    {
+        nome: 'Wayne Barrett',
+        ruolo: 'Founder & CEO',
+        foto : '',
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto : '',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto : '',
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto : '',
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto : '',
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto : '',
+    }
+]
 
-
-
+for (let i = 0; i < membriTeam.length; i++) {
+    const membroSingolo = membriTeam[i]
+    console.log(membroSingolo.nome, membroSingolo.ruolo, membroSingolo.foto)
+}
 
 
 
@@ -12,12 +46,15 @@
 
 /*
 Per la pagina ‘Chi siamo’ del nostro sito aziendale, dobbiamo sviluppare la sezione “Il nostro team”.
-Definiamo un array di oggetti che rappresentano i membri del team.
-Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+Definiamo un array di oggetti che rappresentano i membri del team. X
+Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto. X
+
 Milestone 1:
-stampare in console l’elenco dei membri del team, scrivendo separatamente i dettagli di ciascun componente.
+stampare in console l’elenco dei membri del team, scrivendo separatamente i dettagli di ciascun componente
+
 Milestone 2:
 stampare i dati all’interno di un contenitore nella pagina html in modo dinamico, creando per ciascun membro del team un elemento html che conterrà i suoi dati.
+
 BONUS:
 stilizziamo la sezione realizzando le card di ciascun membro del team, come nel mockup allegato.
 */
